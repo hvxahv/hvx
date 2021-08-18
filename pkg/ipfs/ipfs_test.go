fb95797cc620cd359ee1668d245741bd37142b6d
HVTURINGGA <x@disism.com>
2021-08-18 22:17:43 +0800

    Test the new Broadcasts method.

---
+++ pkg/ipfs/ipfs_test.go
@@ -0,0 +1,33 @@
+package ipfs
+
+import (
+	"fmt"
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+	"testing"
+)
+
+
+func TestInitConfig(t *testing.T) {
+
+	home, err := homedir.Dir()
+	cobra.CheckErr(err)
+
+	// Search configs in home directory with name ".hvxahv" (without extension).
+	viper.AddConfigPath(home)
+	viper.SetConfigName(".hvxahv")
+
+	viper.AutomaticEnv()
+
+	// If a configs file is found, read it in.
+	if err2 := viper.ReadInConfig(); err2 == nil {
+		fmt.Fprintln(os.Stderr, "Using configs file:", viper.ConfigFileUsed())
+	}
+}
+
+func TestInitIPFS(t *testing.T) {
+	TestInitConfig(t)
+	InitIPFS()
+}
