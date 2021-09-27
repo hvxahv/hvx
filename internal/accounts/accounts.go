9a6b84f28d1ab5d927c932fc2fcfcb88af28efbe
HVTURINGGA <x@disism.com>
2021-09-27 21:29:32 +0800

    Complete the account system rewrite.

--- internal/accounts/accounts.go
+++ internal/accounts/accounts.go
@@ -1,13 +1,10 @@
 package accounts
 
 import (
-	"fmt"
 	"github.com/disism/hvxahv/pkg/cockroach"
-	"github.com/disism/hvxahv/pkg/matrix"
 	"github.com/disism/hvxahv/pkg/security"
 	"github.com/go-playground/validator/v10"
 	"github.com/pkg/errors"
-	"github.com/spf13/viper"
 	"gorm.io/gorm"
 	"log"
 )
@@ -15,19 +12,54 @@ import (
 type Accounts struct {
 	gorm.Model
 
-	Username   string `gorm:"primaryKey;type:text;preferredUsername;" validate:"required,min=4,max=16"`
-	Mail       string `gorm:"index;type:text;mail;unique" validate:"required,email"`
-	Password   string `gorm:"type:text;password" validate:"required,min=8,max=100"`
+	Username string `gorm:"primaryKey;type:text;preferredUsername;" validate:"required,min=4,max=16"`
+	Mail     string `gorm:"index;type:text;mail;unique" validate:"required,email"`
+	Password string `gorm:"type:text;password" validate:"required,min=8,max=100"`
 
 	// When creating an account, first verify the username, email address, and password.
 	// After the verification is successful, store the username and key in the actors table,
 	// then use the returned ActorID in this field, and then store the data in the accounts table .
 	// At this time, the context of creating the user is complete.
-	ActorID    uint   `gorm:"type:bigint;actor_id"`
+	ActorID uint `gorm:"type:bigint;actor_id"`
 
 	// Whether to set as a private account
 	IsPrivate  bool   `gorm:"type:boolean;is_private"`
 	PrivateKey string `gorm:"type:text;private_key"`
+}
+
+func (a *Accounts) Delete() error {
+	db := cockroach.GetDB()
+
+	if err := db.Debug().Table("accounts").Where("username = ?", a.Username).Unscoped().Delete(&Accounts{}).Error; err != nil {
+		return err
+	}
+
+	if err := db.Debug().Table("actors").Where("id = ?", a.ActorID).Unscoped().Delete(&Accounts{}).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewAcctNameANDActorID(username string, id uint) *Accounts {
+	return &Accounts{
+		Username: username,
+		ActorID:  id,
+	}
+}
+
+func (a *Accounts) ChangeUsername(target string) error {
+	db := cockroach.GetDB()
+
+	if err := db.Debug().Table("accounts").Where("username = ?", a.Username).Update("username", target).Error; err != nil {
+		return err
+	}
+
+	if err := db.Debug().Table("actors").Where("id = ?", a.ActorID).Update("preferred_username", target).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (a *Accounts) Update() error {
@@ -103,104 +135,7 @@ func (a *Accounts) New() error {
 		return errors.Errorf("FAILED_TO_CREATE_ACCOUNT")
 	}
 
-	if err := db.Debug().Table("accounts").Create(&a).Error; err != nil {
-		return errors.Errorf("FAILED_TO_CREATE_ACCOUNT")
-	}
-
 	return nil
-}
-
-type Actors struct {
-	gorm.Model
-
-	PreferredUsername string `gorm:"primaryKey;type:text;preferredUsername;"`
-	Domain            string `gorm:"type:text;domain"`
-	Avatar            string `gorm:"type:text;avatar"`
-	Name              string `gorm:"type:text;name"`
-	Summary           string `gorm:"type:text;summary"`
-	Inbox             string `gorm:"type:text;inbox"`
-	PublicKey         string `gorm:"type:text;public_key"`
-
-	// ID returned after completing the registration of the matrix account.
-	MatrixID    string `gorm:"type:text;matrix_id;unique"`
-	MatrixToken string `gorm:"type:text;matrix_token"`
-
-	// Whether it is a robot or other type of account
-	ActorType string `gorm:"type:text;actor_type"`
-}
-
-func NewActorID(id uint) *Actors {
-	return &Actors{
-		Model: gorm.Model{
-			ID: id,
-		},
-	}
-}
-
-func (a *Actors) FindByID() (*Actors, error) {
-	db := cockroach.GetDB()
-
-	if err := db.Debug().Table("actors").Where("id = ?", a.ID).First(&a).Error; err != nil {
-		return nil, err
-	}
-
-	return a, nil
-}
-
-func NewActorsPreferredUsername(preferredUsername string) *Actors {
-	return &Actors{PreferredUsername: preferredUsername}
-}
-
-func (a *Actors) FindByPreferredUsername() (*[]Actors, error) {
-	db := cockroach.GetDB()
-
-	var ac []Actors
-	if err := db.Debug().Table("actors").Where("preferred_username = ?", a.PreferredUsername).Find(&ac).Error; err != nil {
-		return nil, err
-	}
-
-	return &ac, nil
-}
-
-func NewActors(preferredUsername, password, publicKey string) *Actors {
-	domain := viper.GetString("localhost")
-
-	id, err := matrix.NewAuth(preferredUsername, password).Register()
-	if err != nil {
-		log.Println("Failed to register to the matrix account.")
-	}
-
-	return &Actors{
-		PreferredUsername: preferredUsername,
-		Domain:            domain,
-		Inbox:             fmt.Sprintf("https://%s/u/%s/inbox", domain, preferredUsername),
-		PublicKey:         publicKey,
-		MatrixID:          id,
-	}
-}
-
-func (a *Actors) NewActor() (uint, error) {
-	db := cockroach.GetDB()
-
-	if err := db.AutoMigrate(&Actors{}); err != nil {
-		return 0, errors.New("FAILED_TO_AUTOMATICALLY_CREATE_DATABASE")
-	}
-
-	if err := db.Debug().Table("actors").Create(&a).Error; err != nil {
-		return 0, errors.Errorf("FAILED_TO_CREATE_ACTOR")
-	}
-
-	return a.ID, nil
-}
-
-type Actor interface {
-	// NewActor Create new actors data and add the returned ID to the accounts field.
-	NewActor() (uint, error)
-
-	// FindByPreferredUsername Find the Actor collection by PreferredUsername.
-	FindByPreferredUsername() (*[]Actors, error)
-
-	FindByID() (*Actors, error)
 }
 
 type Account interface {
@@ -210,4 +145,9 @@ type Account interface {
 	FindByName() (*Accounts, error)
 
 	Update() error
+
+	// ChangeUsername Change the username and pass the target username as a parameter.
+	ChangeUsername(target string) error
+
+	Delete() error
 }
---
+++ internal/accounts/actors.go
@@ -0,0 +1,117 @@
+package accounts
+
+import (
+	"fmt"
+	"github.com/disism/hvxahv/pkg/cockroach"
+	"github.com/disism/hvxahv/pkg/matrix"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
+	"log"
+)
+
+type Actors struct {
+	gorm.Model
+
+	PreferredUsername string `gorm:"primaryKey;type:text;preferredUsername;"`
+	Domain            string `gorm:"type:text;domain"`
+	Avatar            string `gorm:"type:text;avatar"`
+	Name              string `gorm:"type:text;name"`
+	Summary           string `gorm:"type:text;summary"`
+	Inbox             string `gorm:"type:text;inbox"`
+	PublicKey         string `gorm:"type:text;public_key"`
+
+	// ID returned after completing the registration of the matrix account.
+	MatrixID    string `gorm:"type:text;matrix_id;unique"`
+	MatrixToken string `gorm:"type:text;matrix_token"`
+
+	// Whether it is a robot or other type of account
+	ActorType string `gorm:"type:text;actor_type"`
+}
+
+func (a *Actors) Update() error {
+	db := cockroach.GetDB()
+
+	err := db.Debug().Table("actors").Where("id = ?", a.ID).Updates(&a).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewActorID(id uint) *Actors {
+	return &Actors{
+		Model: gorm.Model{
+			ID: id,
+		},
+	}
+}
+
+func (a *Actors) FindByID() (*Actors, error) {
+	db := cockroach.GetDB()
+
+	if err := db.Debug().Table("actors").Where("id = ?", a.ID).First(&a).Error; err != nil {
+		return nil, err
+	}
+
+	return a, nil
+}
+
+func NewActorsPreferredUsername(preferredUsername string) *Actors {
+	return &Actors{PreferredUsername: preferredUsername}
+}
+
+func (a *Actors) FindByPreferredUsername() (*[]Actors, error) {
+	db := cockroach.GetDB()
+
+	var ac []Actors
+	if err := db.Debug().Table("actors").Where("preferred_username = ?", a.PreferredUsername).Find(&ac).Error; err != nil {
+		return nil, err
+	}
+
+	return &ac, nil
+}
+
+func NewActors(preferredUsername, password, publicKey string) *Actors {
+	domain := viper.GetString("localhost")
+
+	id, err := matrix.NewAuth(preferredUsername, password).Register()
+	if err != nil {
+		log.Println("FAILED TO REGISTER TO THE MATRIX ACCOUNT.")
+	}
+
+	return &Actors{
+		PreferredUsername: preferredUsername,
+		Domain:            domain,
+		Inbox:             fmt.Sprintf("https://%s/u/%s/inbox", domain, preferredUsername),
+		PublicKey:         publicKey,
+		MatrixID:          id,
+	}
+}
+
+func (a *Actors) NewActor() (uint, error) {
+	db := cockroach.GetDB()
+
+	if err := db.AutoMigrate(&Actors{}); err != nil {
+		return 0, errors.New("FAILED_TO_AUTOMATICALLY_CREATE_DATABASE")
+	}
+
+	if err := db.Debug().Table("actors").Create(&a).Error; err != nil {
+		return 0, errors.Errorf("FAILED_TO_CREATE_ACTOR")
+	}
+
+	return a.ID, nil
+}
+
+type Actor interface {
+	// NewActor Create new actors data and add the returned ID to the accounts field.
+	NewActor() (uint, error)
+
+	// FindByPreferredUsername Find the Actor collection by PreferredUsername.
+	FindByPreferredUsername() (*[]Actors, error)
+
+	FindByID() (*Actors, error)
+
+	Update() error
+}
