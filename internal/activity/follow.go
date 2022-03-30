0854f8bd7b8251cadf75171de0919ab0165130c2
N.HVTURINGGA <x@disism.com>
2022-03-30 07:17:45 +0000

    add(feature)[activity] follow interface implement.

--- internal/activity/follow.go
+++ internal/activity/follow.go
@@ -1,6 +1,10 @@
 package activity
 
-import "gorm.io/gorm"
+import (
+	"github.com/hvxahv/hvxahv/pkg/cockroach"
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
+)
 
 type Follows struct {
 	gorm.Model
@@ -12,28 +16,88 @@ type Follows struct {
 	Following uint `gorm:"primaryKey;bigint;following"`
 }
 
-func (f Follows) Create() error {
-	//TODO implement me
-	panic("implement me")
+type Friends struct {
+	gorm.Model
+
+	// Friend actor id.
+	Friend uint `gorm:"primaryKey;bigint;friend"`
+
+	// Friend of actor id.
+	FriendOf uint `gorm:"primaryKey;bigint;friend_of"`
 }
 
-func (f Follows) Delete() error {
-	//TODO implement me
-	panic("implement me")
+const (
+	// FollowsTableName is the table name for the Follows table.
+	FollowsTableName = "follows"
+)
+
+func (f *Follows) Create() error {
+	db := cockroach.GetDB()
+	if err := db.AutoMigrate(&Follows{}); err != nil {
+		return err
+	}
+
+	if err := db.Debug().
+		Table(FollowsTableName).
+		Where("follower = ? AND Following = ?", f.Follower, f.Following); err != nil {
+		ok := cockroach.IsNotFound(err.Error)
+		if ok {
+			return errors.New("FOLLOW_ALREADY_EXISTS")
+		}
+	}
+
+	if err := db.Debug().Table("follows").Create(&f).Error; err != nil {
+		return err
+	}
+	return nil
 }
 
-func (f Follows) GetFollowers() ([]uint, error) {
-	//TODO implement me
-	panic("implement me")
+func (f *Follows) Delete() error {
+	db := cockroach.GetDB()
+	if err := db.Debug().Table(FollowsTableName).
+		Where("follower = ? AND Following = ?", f.Follower, f.Following).
+		Unscoped().
+		Delete(&Follows{}).
+		Error; err != nil {
+		return err
+	}
+	return nil
 }
 
-func (f Follows) GetFollowings() ([]uint, error) {
-	//TODO implement me
-	panic("implement me")
+func (f *Follows) GetFollowers() ([]uint, error) {
+	db := cockroach.GetDB()
+	var followers []uint
+	if err := db.Debug().Table(FollowsTableName).
+		Where("follower = ?", f.Follower).
+		Pluck("following", &followers).
+		Error; err != nil {
+		return nil, err
+	}
+	return followers, nil
+}
+
+func (f *Follows) GetFollowings() ([]uint, error) {
+	db := cockroach.GetDB()
+	var followings []uint
+	if err := db.Debug().Table(FollowsTableName).
+		Where("following = ?", f.Following).
+		Pluck("follower", &followings).
+		Error; err != nil {
+		return nil, err
+	}
+	return followings, nil
 }
 
 func NewFollows(follower uint, following uint) *Follows {
 	return &Follows{Follower: follower, Following: following}
+}
+
+func NewFollower(follower uint) *Follows {
+	return &Follows{Follower: follower}
+}
+
+func NewFollowing(following uint) *Follows {
+	return &Follows{Following: following}
 }
 
 type Followee interface {
--- internal/activity/inbox_test.go
+++ internal/activity/inbox_test.go
@@ -2,115 +2,40 @@ package activity
 
 import (
 	"fmt"
-	pb "github.com/hvxahv/hvxahv/api/activity/v1alpha1"
-	"golang.org/x/net/context"
 	"testing"
 )
 
-func TestActivity_InboxForFollow(t *testing.T) {
-	name := "hvxahv"
-	follow := []byte(`{
-		"@context":"https://www.w3.org/ns/activitystreams",
-		"id":"https://mas.to/e27a4e0e-a0a0-400e-a395-6b0e60f08291",
-		"type":"Follow",
-		"actor":"https://mas.to/users/hvturingga",
-		"object":"https://07ee-2408-832f-20b2-be60-7c3c-bb0d-7b8b-bb20.ngrok.io/u/hvturingga"
-	}`)
-
-	s := &activity{}
-	inbox, err := s.Inbox(context.Background(), &pb.InboxRequest{
-		Name: name,
-		Data: follow,
-	})
-	if err != nil {
-		return
+func TestFollows_Create(t *testing.T) {
+	fo := []uint{746932029260431361, 747232968277196801, 747524801241219073}
+	for _, i := range fo {
+		if err := NewFollows(746931986864701441, i).Create(); err != nil {
+			t.Error(err)
+			return
+		}
 	}
-	fmt.Println(inbox)
 }
 
-func TestActivity_InboxForUndo(t *testing.T) {
-	name := "hvxahv"
-	undo := []byte(`{
-	  "@context": "https://www.w3.org/ns/activitystreams",
-	  "summary": "Sally retracted her offer to John",
-	  "type": "Undo",
-	  "actor": "https://mas.to/users/hvturingga",
-	  "object": {
-		"type": "Follow",
-		"actor": "https://halfmemories.com/u/hvturingga",
-		"object": "https://mas.to/e27a4e0e-a0a0-400e-a395-6b0e60f08291"
-	  }
-	}`)
-	s := &activity{}
-	inbox, err := s.Inbox(context.Background(), &pb.InboxRequest{
-		Name: name,
-		Data: undo,
-	})
+func TestFollows_Delete(t *testing.T) {
+	if err := NewFollows(747524801241219073, 747232968277196801).Delete(); err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestFollows_GetFollowers(t *testing.T) {
+	x, err := NewFollower(746931986864701441).GetFollowers()
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	fmt.Println(inbox)
+	fmt.Println(x)
 }
 
-func TestActivity_InboxForAccept(t *testing.T) {
-	name := "hvxahv"
-	accept := []byte(`{
-	  "@context": "https://www.w3.org/ns/activitystreams",
-	  "summary": "Sally accepted an invitation to a party",
-	  "type": "Accept",
-	  "actor": "https://mas.to/users/hvturingga",
-	  "object": {
-		"type": "Invite",
-		"actor": "https://halfmemories.com/u/hvturingga",
-		"object": "https://mas.to/e27a4e0e-a0a0-400e-a395-6b0e60f08291"
-	  }
-	}`)
-	s := &activity{}
-	inbox, err := s.Inbox(context.Background(), &pb.InboxRequest{
-		Name: name,
-		Data: accept,
-	})
+func TestFollows_GetFollowing(t *testing.T) {
+	x, err := NewFollowing(747524801241219073).GetFollowings()
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	fmt.Println(inbox)
-}
-
-func TestActivity_GetInboxByActivityID(t *testing.T) {
-	s := &activity{}
-	inbox, err := s.GetInboxByActivityID(context.Background(), &pb.GetInboxByActivityIDRequest{
-		ActivityId: "748289222292602881",
-	})
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	fmt.Println(inbox)
-}
-
-func TestActivity_GetInboxesByAccountID(t *testing.T) {
-	s := &activity{}
-	inboxes, err := s.GetInboxesByAccountID(context.Background(), &pb.GetInboxesByAccountIDRequest{
-		AccountId: "746931986864701441",
-	})
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	fmt.Println(inboxes)
-}
-
-func TestActivity_DeleteInboxByActivityID(t *testing.T) {
-	s := &activity{}
-	ibx, err := s.DeleteInboxByInboxesID(context.Background(), &pb.DeleteInboxByInboxesIDRequest{
-		ActivityId: "748289253916311553",
-		AccountId:  "746931986864701441",
-	})
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	fmt.Println(ibx)
+	fmt.Println(x)
 }
